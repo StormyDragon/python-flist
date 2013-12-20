17a99ff95d2a267ab84c0b8deb94e7360564b377
Forever Typing <forevertyping@outlook.com>
2013-12-20 22:51:14 +0000

    Apply PEP8

--- flist/core.py
+++ flist/core.py
@@ -1,9 +1,9 @@
 import weakref
 import api
 import fchat
+import logging
 
-import logging as log
-logging = log.getLogger("flist.core")
+logger = logging.getLogger("flist")
 
 class AccountMissingException(Exception):
     pass
@@ -12,27 +12,27 @@ class FList_Character():
     def __init__(self, charactername, account):
         self.charname = charactername
         self._account = weakref.ref(account)
-    
+
     @property
     def account(self):
         val = self._account()
         if not val:
             raise AccountMissingException("This character has no account associated to it.")
         return val
-    
+
     def start_chat(self, **kwargs):
-        return fchat.FChat(self, **kwargs)
-    
+        return fchat.Connection(self, **kwargs)
+
     def __unicode__(self):
         return self.charname
 
-#
+
 class FList_Account():
     def __init__(self, accountname, password):
         self.characters = {}
         self.account = accountname
         self.refresh(password)
-    
+
     def refresh(self, password):
         data = api.get_ticket(self.account, password)
         self.bookmarks = data['bookmarks']
@@ -41,7 +41,7 @@ class FList_Account():
         for charname in data['characters']:
             c = FList_Character(charname, self)
             self.characters.setdefault(charname, c) # Update the character.
-        
+
     def get_ticket(self):
         return self.ticket
 
