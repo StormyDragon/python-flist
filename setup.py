22dc21d17b11ec2330b7ac873955966c06df6bec
StormyDragon <stormy-git@stormweyr.dk>
2014-01-08 23:52:46 +0100

    setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Python-flist',
+      version='0.1.0',
+      author='StormyDragon',
+      author_email='stormy.py@stormweyr.dk',
+      description='Python module for interacting with the f-list website.',
+      url='https://github.com/StormyDragon/python-flist',
+      packages=['flist',]
+)
