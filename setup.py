4d3fbb962f67736712598631771b6f889191d930
StormyDragon <stormy-git@stormweyr.dk>
2016-05-31 08:33:16 +0200

    Update versions

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@
 from distutils.core import setup
 import sys
 
-py_version = sys.version_info[:2]
-
 additional_requirements = []
-if py_version == (3, 3):
-    additional_requirements.append('asyncio')
 
 setup(name='Python-flist',
       version='0.1.0',
@@ -22,8 +18,7 @@ setup(name='Python-flist',
           "License :: OSI Approved :: BSD License",
           "Intended Audience :: Developers",
           "Programming Language :: Python",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
           "Operating System :: OS Independent",
           "Topic :: Games/Entertainment :: Role-Playing",
           "Topic :: Communications :: Chat",
@@ -33,7 +28,7 @@ setup(name='Python-flist',
       requires=[
           'websockets (>=2.4)',
           'aiohttp (>=0.16.6)',
-      ] + additional_requirements,
+      ],
       provides=[
           'flist',
       ],
