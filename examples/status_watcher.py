5a531923246f15dc42d690fb6b2b4fa4322891e2
StormyDragon <stormy-git@stormweyr.dk>
2016-09-15 17:36:51 +0200

    Simplify main method, wait until status logger completes
    Hint: It never will.

--- examples/status_watcher.py
+++ examples/status_watcher.py
@@ -1,6 +1,7 @@
 import logging
 from flist import account_login, start_chat, opcode
 import asyncio
+from sys import argv
 
 logger = logging.getLogger('status_watcher')
 logging.getLogger('').setLevel('DEBUG')
@@ -12,8 +13,12 @@ async def log_status_async(status_provider):
 async def connect(account, password, character_name):
     account = await account_login(account, password)
     character = account.get_character(character_name)
-    logger.info("Starting chat.")
     chat = await start_chat(character)
+    return chat
+
+async def status_logger():
+    logger.info("Starting chat.")
+    chat = await connect(argv[1], argv[2], argv[3])
     logger.info("Attaching log_status method.")
     status_provider = chat.watch(opcode.STATUS)
     await log_status_async(status_provider)
@@ -21,6 +26,5 @@ async def connect(account, password, character_name):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
     logger.setLevel(logging.INFO)
-    from sys import argv
-    coroutine = connect(argv[1], argv[2], argv[3])
-    asyncio.get_event_loop().run_until_complete(coroutine)
+
+    asyncio.get_event_loop().run_until_complete(status_logger())
