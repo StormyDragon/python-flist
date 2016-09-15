22e1e4d85843540716be4221f8303f0cfb4633da
StormyDragon <stormy-git@stormweyr.dk>
2016-09-15 18:10:31 +0200

    Code reformatting

--- examples/status_watcher.py
+++ examples/status_watcher.py
@@ -1,14 +1,17 @@
-import logging
-from flist import account_login, start_chat, opcode
 import asyncio
+import logging
 from sys import argv
+
+from flist import account_login, start_chat, opcode
 
 logger = logging.getLogger('status_watcher')
 logging.getLogger('').setLevel('DEBUG')
 
+
 async def log_status_async(status_provider):
     async for message in status_provider:
         logger.info("%(character)s is %(status)s: %(statusmsg)s", message)
+
 
 async def connect(account, password, character_name):
     account = await account_login(account, password)
@@ -16,12 +19,14 @@ async def connect(account, password, character_name):
     chat = await start_chat(character)
     return chat
 
+
 async def status_logger():
     logger.info("Starting chat.")
     chat = await connect(argv[1], argv[2], argv[3])
     logger.info("Attaching log_status method.")
     status_provider = chat.watch(opcode.STATUS)
     await log_status_async(status_provider)
+
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
