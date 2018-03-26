fac6b7c48bdb47ba9fc04bce169b6bbd3d0c4fae
StormyDragon <stormy-git@stormweyr.dk>
2018-03-27 00:41:41 +0200

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 ]
 
 setup(name='Python-flist',
-      version='0.1.0',
+      version='0.1.1',
       author='StormyDragon',
       author_email='stormy.pypi@stormweyr.dk',
       description='Python module for interacting with the f-list website.',
