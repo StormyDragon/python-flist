f83e09fd0b2df895aab68e86b7584ca0bf5f457c
StormyDragon <stormy-git@stormweyr.dk>
2019-05-17 02:56:21 +0200

    pin dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 ]
 
 setup(name='Python-flist',
-      version='0.1.2',
+      version='0.1.3',
       author='StormyDragon',
       author_email='stormy.pypi@stormweyr.dk',
       description='Python module for interacting with the f-list website.',
